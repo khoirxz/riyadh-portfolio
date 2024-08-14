acfa2d63d3cbc0f98574880e227785f1e34c2f8a
khoirrurrohman <khoirrurrohman@gmail.com>
2024-08-14 10:06:20 +0000

    initialize tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="font-oswald">
+      <h1>Hello, world!</h1>
+      <p>This is my first React app.</p>
+    </div>
+  )
+}
+
+export default App
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:320877342.
