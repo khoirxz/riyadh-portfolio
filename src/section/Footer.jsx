6a789f871e3e306430a3555d4243a8223ae19f9e
khoirxz <khoirrurrohman@gmail.com>
2024-08-21 19:51:02 +0700

    add footer

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -1,7 +1,16 @@
 const Footer = () => {
   return (
-    <footer className="text-sm border-t border-gray-200 dark:border-neutral-700 py-8 text-center font-figtree">
-      <p>Copyright &copy; {new Date().getFullYear()} Riyad.</p>
+    <footer
+      className="relative min-h-[600px]"
+      style={{
+        clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)",
+      }}>
+      <div className="fixed bottom-0 min-h-[600px] w-full bg-yellow-200 text-black p-5">
+        <h1 className="text-2xl text-center">Contact me</h1>
+        <p className="font-figtree text-sm font-semibold">
+          Copyright &copy; {new Date().getFullYear()} Riyad.
+        </p>
+      </div>
     </footer>
   );
 };
